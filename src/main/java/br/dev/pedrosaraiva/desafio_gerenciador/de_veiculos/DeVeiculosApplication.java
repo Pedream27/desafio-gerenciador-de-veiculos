a37576d9d00bdedce9bbc201fb88a7a8c762c3e6
Pedro Henrique Saraiva <pedreamsaraiva@gmail.com>
2024-06-22 12:09:45 -0300

    iniciando projeto

---
+++ src/main/java/br/dev/pedrosaraiva/desafio_gerenciador/de_veiculos/DeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pedrosaraiva.desafio_gerenciador.de_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/pedrosaraiva/desafio_gerenciador/de_veiculos/DeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.pedrosaraiva.desafio_gerenciador.de_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
